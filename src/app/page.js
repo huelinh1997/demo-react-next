b01f39ff614ccd22251d7dfbbba96fcb443e2db8
Nguyen Hue Linh <linhnh@tech.est-rouge.com>
2023-11-02 00:13:25 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,170 @@
-import Image from 'next/image'
+"use client"
+import { useEffect, useRef, useState } from 'react';
+import { 
+  List, 
+  AutoSizer, 
+  CellMeasurer,
+  CellMeasurerCache,
+  InfiniteLoader,
+} from 'react-virtualized';
 
 export default function Home() {
+  const [people, setPeople] = useState([]);
+  const [time, setTime] = useState(new Date());
+  const [loading, setLoading] = useState(new Date());
+
+  const cache = useRef( new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 100,
+  }));
+
+  useEffect(()=> {
+    setPeople([...Array(15).keys()].map(key => ({
+      id: key,
+      name: `Kian Ebert ${key}`,
+      bio: 'Exercitationem molestiae autem dignissimos ratione sunt ut temporibus. Modi iusto quos molestias. Aut ab fugit id ullam amet commodi iusto est. Itaque qui minus. Animi velit iure est at corrupti expedita deserunt. Reprehenderit non ab nihil aut aut est sint quas sunt. Doloremque ullam quod ut aut ut qui. Sapiente quod non. In praesentium quas porro aliquam eius. Aut voluptatum ea. Eum sit illo maiores libero consequuntur. Qui eius suscipit est voluptatibus consequatur omnis. Doloribus et maxime. Nisi sint quia velit rerum beatae et eum. Et dolores quibusdam vitae magni dicta voluptate corrupti tempora. Minima nihil iusto est delectus enim. Temporibus odit itaque ut est sunt. Sed debitis illo et et corrupti iste molestiae magnam. Aut eos qui. Id eveniet est blanditiis quo. Suscipit rem voluptatem. Consequatur architecto dolor. Vero magnam magni rerum et laboriosam veritatis non. Est optio eos qui eaque ut. Eveniet ad sunt molestias sunt aut voluptas. Blanditiis doloribus earum. Itaque voluptatem inventore. Et quod et fuga quis nisi ea ipsam fugiat delectus. Fuga debitis omnis nostrum rerum iste nihil qui et. Qui aut voluptatem iste asperiores. Aperiam pariatur veniam nobis consequuntur quis. Consequatur explicabo veniam commodi earum temporibus non est dicta. Qui dolor et et sit. Et et alias. Ut sunt sapiente ullam. Quis corporis aut sit reiciendis omnis. Fugiat possimus quaerat. Odit cumque consequatur ut reprehenderit nihil perspiciatis. Qui repellat voluptatem aperiam nam veniam voluptatem natus aspernatur laudantium. Eum sunt repudiandae. Architecto sit cupiditate.'
+    })));
+
+    const interval = setInterval(()=> {
+      setTime(new Date())
+    }, 1000)
+    
+    return () => clearInterval(interval);
+  }, [])
+
+  function isRowLoaded ({ index }) {
+    return !!people[index];
+  }
+
+  const fetchData = (index) => {
+    setLoading(true);
+    setTimeout(()=> {
+      const newData = [...Array(3).keys()].map(key => ({
+        id: key,
+        name: `Kian Ebert ${index + key}`,
+        bio: 'Exercitationem molestiae autem dignissimos ratione sunt ut temporibus. Modi iusto quos molestias. Aut ab fugit id ullam amet commodi iusto est. Itaque qui minus. Animi velit iure est at corrupti expedita deserunt. Reprehenderit non ab nihil aut aut est sint quas sunt. Doloremque ullam quod ut aut ut qui. Sapiente quod non. In praesentium quas porro aliquam eius. Aut voluptatum ea. Eum sit illo maiores libero consequuntur. Qui eius suscipit est voluptatibus consequatur omnis. Doloribus et maxime. Nisi sint quia velit rerum beatae et eum. Et dolores quibusdam vitae magni dicta voluptate corrupti tempora. Minima nihil iusto est delectus enim. Temporibus odit itaque ut est sunt. Sed debitis illo et et corrupti iste molestiae magnam. Aut eos qui. Id eveniet est blanditiis quo. Suscipit rem voluptatem. Consequatur architecto dolor. Vero magnam magni rerum et laboriosam veritatis non. Est optio eos qui eaque ut. Eveniet ad sunt molestias sunt aut voluptas. Blanditiis doloribus earum. Itaque voluptatem inventore. Et quod et fuga quis nisi ea ipsam fugiat delectus. Fuga debitis omnis nostrum rerum iste nihil qui et. Qui aut voluptatem iste asperiores. Aperiam pariatur veniam nobis consequuntur quis. Consequatur explicabo veniam commodi earum temporibus non est dicta. Qui dolor et et sit. Et et alias. Ut sunt sapiente ullam. Quis corporis aut sit reiciendis omnis. Fugiat possimus quaerat. Odit cumque consequatur ut reprehenderit nihil perspiciatis. Qui repellat voluptatem aperiam nam veniam voluptatem natus aspernatur laudantium. Eum sunt repudiandae. Architecto sit cupiditate.'
+      }));
+      setPeople([...people, newData])
+      setLoading(false);
+    }, 2000)
+  }
+  let itemStatusMap = {};
+  const LOADING = 1;
+  const LOADED = 2;
+  const remoteRowCount = 100;
+
+  const loadMoreRows = async ({ startIndex, stopIndex }) => {
+    console.log('startIndex:', startIndex);
+    console.log('stopIndex:', stopIndex);
+    for (let index = startIndex; index <= stopIndex; index++) {
+      itemStatusMap[index] = LOADING;
+    }
+  
+    await new Promise(() => setTimeout(()=>{}, 1000));
+  
+    for (let index = startIndex; index <= stopIndex; index++) {
+      
+      itemStatusMap[index] = LOADED;
+      console.log('loaded:', itemStatusMap[index]);
+    }
+    // Simulate an API request to load more data
+    // setTimeout(()=> {
+    //   const newData = [...Array(3).keys()].map(key => ({
+    //     id: key,
+    //     name: `Kian Ebert ${startIndex || 0 + key}`,
+    //     bio: 'Exercitationem molestiae autem dignissimos ratione sunt ut temporibus. Modi iusto quos molestias. Aut ab fugit id ullam amet commodi iusto est. Itaque qui minus. Animi velit iure est at corrupti expedita deserunt. Reprehenderit non ab nihil aut aut est sint quas sunt. Doloremque ullam quod ut aut ut qui. Sapiente quod non. In praesentium quas porro aliquam eius. Aut voluptatum ea. Eum sit illo maiores libero consequuntur. Qui eius suscipit est voluptatibus consequatur omnis. Doloribus et maxime. Nisi sint quia velit rerum beatae et eum. Et dolores quibusdam vitae magni dicta voluptate corrupti tempora. Minima nihil iusto est delectus enim. Temporibus odit itaque ut est sunt. Sed debitis illo et et corrupti iste molestiae magnam. Aut eos qui. Id eveniet est blanditiis quo. Suscipit rem voluptatem. Consequatur architecto dolor. Vero magnam magni rerum et laboriosam veritatis non. Est optio eos qui eaque ut. Eveniet ad sunt molestias sunt aut voluptas. Blanditiis doloribus earum. Itaque voluptatem inventore. Et quod et fuga quis nisi ea ipsam fugiat delectus. Fuga debitis omnis nostrum rerum iste nihil qui et. Qui aut voluptatem iste asperiores. Aperiam pariatur veniam nobis consequuntur quis. Consequatur explicabo veniam commodi earum temporibus non est dicta. Qui dolor et et sit. Et et alias. Ut sunt sapiente ullam. Quis corporis aut sit reiciendis omnis. Fugiat possimus quaerat. Odit cumque consequatur ut reprehenderit nihil perspiciatis. Qui repellat voluptatem aperiam nam veniam voluptatem natus aspernatur laudantium. Eum sunt repudiandae. Architecto sit cupiditate.'
+    //   }));
+    //   console.log('newData:', newData);
+    //   setPeople([...people, newData])
+    //   setLoading(false);
+    // }, 2000);
+  };
+
+  const rowRenderer = ({ key, index, style }) => {
+    const person = people[index];
+    let content;
+    console.log('itemStatusMapppp:', itemStatusMap);
+    if (itemStatusMap[index] === LOADED) {
+      content = (
+        <>
+          <h2 className='font-bold mt-2'>{person?.name}</h2>
+          <p>{person.bio}</p>
+        </>
+      );
+    } else {
+      content = "Loading...";
+    }
+    
+    return (
+      <CellMeasurer 
+        cache={cache?.current}
+        rowIndex={index}
+        columnIndex={0}
+        parent={parent}
+      >
+      <div key={key} style={style}>
+        {content}
+      </div>
+    </CellMeasurer>
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <p>{time.toISOString()}</p>
+      <div style={{width: '100%', height: '100vh'}}>
+        <AutoSizer>
+          {
+            ({width, height})=> (
+              <InfiniteLoader
+              rowCount={remoteRowCount}
+                isRowLoaded={isRowLoaded}
+                loadMoreRows={loadMoreRows}
+              >
+                {({ onRowsRendered, registerChild }) => (
+                  <List 
+                    ref={registerChild}
+                    width={width} 
+                    height={height}
+                    rowCount={people.length}
+                    rowHeight={cache?.current?.rowHeight}
+                    deferredMeasurementCache={cache?.current}
+                    onRowsRendered={onRowsRendered}
+                    rowRenderer={rowRenderer}
+                    // rowRenderer={({key,index, style, parent})=> {
+                    //   const person = people[index]
+                    //   return (
+                    //   <CellMeasurer 
+                    //       cache={cache?.current}
+                    //       rowIndex={index}
+                    //       columnIndex={0}
+                    //       parent={parent}
+                    //     >
+                    //     <div key={key} style={style}>
+                    //       <h2 className='font-bold mt-2'>{person?.name}</h2>
+                    //       <p>{person.bio}</p>
+                    //     </div>
+                    //   </CellMeasurer>
+                    //   )
+                    // }}
+                    //onRowsRendered={onRowsRendered}
+                  />
+                )}
+              </InfiniteLoader>
+              
+            )
+          }
+        </AutoSizer>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      
+      {/* <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <h2 className='font-bold mt-2'>{person.name}</h2>
+            <p>{person.bio}</p>
+          </li>
+        ))}
+      </ul> */}
+    </div>
   )
 }
